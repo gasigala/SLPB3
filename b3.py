bda0bbf8a823c41690bb55a41f7dd5a81240779c
DimitriLyon <70168097+DimitriLyon@users.noreply.github.com>
2022-02-08 14:51:11 -0700

    Trained the model

--- b3.py
+++ b3.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 
 def makeDict():
     #make string of all letters
-    letters = string.ascii_lowercase
+    letters = string.ascii_lowercase + "áéíóúüöß'ìã"
 
     #add all bigrams to string 
     b_list = [i+b for i in letters for b in letters]
@@ -19,8 +19,12 @@ def makeNgrams(ngramsDict, surname, N):
     #this method assumes that the possible combinations of our ngrams are already in
     #our dict
     ngrams =  [surname[i: j] for i in range(len(surname)) for j in range(i + 1, len(surname) + 1) if len(surname[i:j]) == N]
+    count = 0
     for i in ngrams:
         ngramsDict[i] +=1
+        count += 1
+    
+    
 
 def bigram_dict_to_matrix(bigram_dict):
     return np.matrix(list(bigram_dict.values()))
@@ -49,9 +53,9 @@ def trainTestSplit():
   testset = 'test-set.csv'
   
   count_total = 0
-  with open(trainset, mode = "w", encoding = "utf-8") as output_file,\
-      open(devset, mode = "w", encoding = "utf-8") as output_file2,\
-      open(testset, mode = "w", encoding= "utf-8") as output_file3:
+  with open(trainset, mode = "w",encoding = 'utf-8') as output_file,\
+      open(devset, mode = "w",encoding = 'utf-8') as output_file2,\
+      open(testset, mode = "w",encoding = 'utf-8') as output_file3:
     for nationality,count in surname_dict.items():
       #get the first 60% into out train-set.csv
       for i in range(count_total, int(count_total+(count*.6))):
@@ -94,15 +98,71 @@ def normalizeCounts(bigramDict):
   print(bigramDict)
 
 def normalizeMat(bigramMat):
+    if bigramMat.sum() == 0:
+        print(bigramMat.sum())
+    
     return bigramMat/bigramMat.sum()
 
 def train_reg():
-    train_file = open("train-set.csv",'r')
+    train_file = open("train-set.csv",'r',encoding = 'utf-8')
     
     train_list = train_file.read().split()
     
-    return train_list
+    base_dict = makeDict()
+    
+    name = train_list[0]
+    
+    name = name.replace(" ", '')
+    
+    names = name.split(",")
+    names[0] = names[0].lower()
+    if "Russian" == names[1]:
+        y = np.matrix([1])
+    else:
+        y = np.matrix([0])
+    
+    name_dict = dict.copy(base_dict)
+    
+    makeNgrams(name_dict, names[0], 2)
+    
+    X = bigram_dict_to_matrix(name_dict)
+    
+    X = normalizeMat(X)
+    
+    for l in train_list:
+        l = l.replace(" ",'')
+        l = l.replace("-",'')
+        
+        names = l.split(',')
+        names[0] = names[0].lower()
+        
+        if len(names[0]) < 2:
+            continue
+        
+        if len(names) == 2 and "Russian" == names[1]:
+            y = np.concatenate((y,[[1]]),1)
+        else:
+            y = np.concatenate((y,[[0]]),1)
+        
+        name_dict = dict.copy(base_dict)
+        
+        makeNgrams(name_dict, names[0], 2)
+        
+        X1 = bigram_dict_to_matrix(name_dict)
+        
+        X1 = normalizeMat(X1)
+        
+        X = np.concatenate((X,X1))
+    
+    regr = LinearRegression().fit(np.asarray(X),np.asarray(y.transpose()))
+    
+    return regr
 
 
 if __name__ == "__main__":
+    trainTestSplit()
+    train_file = open("train-set.csv",'r',encoding = 'utf-8')
+    r = train_file.read()
+    train_file.close()
     regr = train_reg()
+    print(regr)
